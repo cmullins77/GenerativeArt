f18a9ebdb82015771526074c65da0e6a8b8849cc
Cassie Mullins <mullins.cassie@gmail.com>
2022-04-20 23:57:08 -0500

    Update Base to Work on Laptop

--- Door/Scripts/Base.py
+++ Door/Scripts/Base.py
@@ -1,11 +1,13 @@
 import sys
-path = 'C:/Users/mulli/OneDrive/Documents/GitHub/GenerativeArt/Door/Scripts'
+import importlib
+#path = 'C:/Users/mulli/OneDrive/Documents/GitHub/GenerativeArt/Door/Scripts'
+path = 'C:/Users/Cassie/Documents/GitHub/GenerativeArt/Door/Scripts'
 sys.path.append(path)
 
 import GeneticUI
 import Door
 import Gene
-reload(Door)
-reload(Gene)
-reload(GeneticUI)
+importlib.reload(Door)
+importlib.reload(Gene)
+importlib.reload(GeneticUI)
 GeneticUI.run()
